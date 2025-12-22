1449fd7419842f70426614475a6aaae4597c46af
김성근 <78168345+skkim123@users.noreply.github.com>
2025-12-22 23:23:23 +0900

    강의

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,11 +1,71 @@
-import { Outlet } from "react-router";
+import { Box, styled, Typography } from "@mui/material";
+import { NavLink, Outlet } from "react-router";
+import HomeIcon from "@mui/icons-material/Home";
+
+const Layout = styled("div")({
+  display: "flex",
+  height: "100vh",
+  padding: "8px",
+});
+
+const SideBar = styled("div")(({ theme }) => ({
+  width: "331px",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
+  },
+}));
+
+const ContentBox = styled(Box)(({ theme }) => ({
+  borderRadius: "8px",
+  backgroundColor: theme.palette.background.paper,
+  color: theme.palette.text.primary,
+  width: "100%",
+  padding: "8px",
+  marginBottom: "8px",
+  marginRight: "8px",
+}));
+
+const NavList = styled("ul")(({ theme }) => ({
+  listStyle: "none",
+  padding: "0",
+  margin: "0",
+}));
+
+const StyledNavLink = styled(NavLink)(({ theme }) => ({
+  textDecoration: "none",
+  display: "flex",
+  alignItems: "center",
+  gap: "20px",
+  color: theme.palette.text.secondary,
+  "&:hover": {
+    color: theme.palette.text.primary,
+  },
+  "&.active": {
+    color: theme.palette.text.primary,
+  },
+}));
 
 const AppLayout = () => {
   return (
-    <div>
-      SideBar
+    <Layout>
+      <SideBar>
+        <ContentBox>
+          <NavList>
+            <StyledNavLink to="/">
+              <HomeIcon />
+              <Typography variant="h2" fontWeight={700}>
+                Home
+              </Typography>
+            </StyledNavLink>
+            <StyledNavLink to="/search">Search</StyledNavLink>
+          </NavList>
+        </ContentBox>
+      </SideBar>
       <Outlet />
-    </div>
+    </Layout>
   );
 };
 
