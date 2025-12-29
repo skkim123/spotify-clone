8b99c1757ecb0371a42200e337671650dae2f663
김성근 <78168345+skkim123@users.noreply.github.com>
2025-12-30 01:09:18 +0900

    플레이리스트 2

--- src/hooks/useGetCurrentUserPlaylists.ts
+++ src/hooks/useGetCurrentUserPlaylists.ts
@@ -1,15 +1,23 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import getCurrentUserPlaylists from "../apis/playlistApi";
 import type { GetCurrentUserPlaylistsRequest } from "../models/playlist";
 
 export const useGetCurrentUserPlaylists = ({
   limit,
-  offset,
 }: GetCurrentUserPlaylistsRequest) => {
-  return useQuery({
+  return useInfiniteQuery({
     queryKey: ["current-user-playlists"],
-    queryFn: () => {
-      return getCurrentUserPlaylists({ limit, offset });
+    queryFn: ({ pageParam = 0 }) => {
+      return getCurrentUserPlaylists({ limit, offset: pageParam });
+    },
+    initialPageParam: 0,
+    // getNextPageParam의 리턴값이 queryFn의 pageParam으로 전달됨 !!! 오호
+    getNextPageParam: (lastPage) => {
+      if (lastPage.next) {
+        const url = new URL(lastPage.next);
+        const nextOffset = url.searchParams.get("offset");
+        return nextOffset ? parseInt(nextOffset) : undefined;
+      }
     },
   });
 };
--- src/layout/components/Library.tsx
+++ src/layout/components/Library.tsx
@@ -1,27 +1,128 @@
 import LibraryHead from "./LibraryHead";
 import EmptyPlaylist from "./EmptyPlaylist";
-import styled from "@emotion/styled";
+import { Box, Typography, styled } from "@mui/material";
 import { useGetCurrentUserPlaylists } from "../../hooks/useGetCurrentUserPlaylists";
-import UserPlaylist from "./UserPlaylist";
+import type { SimplifiedPlaylist } from "../../models/playlist";
+import { useInView } from "react-intersection-observer";
+import { useEffect, useState, useRef } from "react";
+import LoadingSpinner from "../../common/components/LoadingSpinner";
 
-const LibraryBox = styled("div")({
+const LibraryBox = styled(Box)({
   display: "flex",
   flexDirection: "column",
   gap: "20px",
 });
 
-const Library = () => {
-  const { data } = useGetCurrentUserPlaylists({
-    limit: 10,
-    offset: 0,
-  });
+const PlaylistContainer = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  gap: "4px",
+  overflowY: "auto",
+  maxHeight: "calc(100vh - 300px)",
+});
 
-  const userPlaylistExists = !!data && data?.items.length > 0;
+const PlaylistItem = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "12px",
+  padding: "8px",
+  borderRadius: "8px",
+  cursor: "pointer",
+});
+
+const PlaylistImage = styled("img")({
+  width: "48px",
+  height: "48px",
+  borderRadius: "4px",
+});
+
+const NoImageBox = styled(Box)({
+  width: "48px",
+  height: "48px",
+  borderRadius: "4px",
+  backgroundColor: "#282828",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const PlaylistInfo = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  gap: "2px",
+  overflow: "hidden",
+});
+
+interface PlaylistProps {
+  playlist: SimplifiedPlaylist;
+}
+
+const Playlist = ({ playlist }: PlaylistProps) => {
+  return (
+    <PlaylistItem>
+      {playlist.images && playlist.images.length > 0 ? (
+        <PlaylistImage src={playlist.images[0].url} alt={playlist.name} />
+      ) : (
+        <NoImageBox>
+          <Typography variant="caption" color="grey">
+            No image
+          </Typography>
+        </NoImageBox>
+      )}
+      <PlaylistInfo>
+        <Typography variant="body1" fontWeight={400} sx={{ color: "white" }}>
+          {playlist.name}
+        </Typography>
+        <Typography variant="body2" noWrap sx={{ color: "grey" }}>
+          Playlist • {playlist.owner?.display_name}
+        </Typography>
+      </PlaylistInfo>
+    </PlaylistItem>
+  );
+};
+
+const Library = () => {
+  const [container, setContainer] = useState<HTMLDivElement | null>(null);
+  const { data, hasNextPage, isFetchingNextPage, fetchNextPage } =
+    useGetCurrentUserPlaylists({
+      limit: 10,
+    });
+  const { ref, inView } = useInView({
+    root: container,
+  });
+  const prevInView = useRef(false);
+
+  const userPlaylistExists = !!data && data?.pages[0].items.length > 0;
+
+  useEffect(() => {
+    // inView가 false → true로 변할 때만 fetch
+    if (inView && !prevInView.current && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+    prevInView.current = inView;
+  }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
 
   return (
     <LibraryBox>
       <LibraryHead />
-      {userPlaylistExists ? <UserPlaylist /> : <EmptyPlaylist />}
+      {userPlaylistExists ? (
+        <PlaylistContainer ref={setContainer}>
+          {data.pages
+            .flatMap((page) => page.items)
+            .map((playlist) => (
+              <Playlist key={playlist.id} playlist={playlist} />
+            ))}
+          <div ref={ref}>
+            {isFetchingNextPage ? (
+              <LoadingSpinner />
+            ) : (
+              <div style={{ height: "1px", backgroundColor: "black" }}></div>
+            )}
+          </div>
+        </PlaylistContainer>
+      ) : (
+        <EmptyPlaylist />
+      )}
     </LibraryBox>
   );
 };
